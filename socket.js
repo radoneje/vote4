c4d6d70435da4dd32e5b86123800a079ac781911
radoneje <53825282+radoneje@users.noreply.github.com>
2023-07-23 23:49:49 +0300

    ee

--- socket.js
+++ socket.js
@@ -4,7 +4,7 @@ const WebSocket = require('ws');
 const url = require('url');
 
 const server=http.createServer()
-const wss = new WebSocketServer({ noServer: true });
+const wss = new WebSocket({ noServer: true });
 server.listen(7001);
 
 ws.on('connection', (connection) => {
