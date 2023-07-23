b0e5c45f9cb0d3eff00ecb3cee3ed8f4172d23be
radoneje <53825282+radoneje@users.noreply.github.com>
2023-07-23 23:58:35 +0300

    ee

--- socket.js
+++ socket.js
@@ -4,7 +4,7 @@ const WebSocket = require('ws');
 const url = require('url');
 
 const server=http.createServer()
-const wss =  WebSocket.Server({ noServer: true });
+const wss =  new WebSocket.Server({ noServer: true });
 server.listen(7001);
 server.on('upgrade', function upgrade(request, socket, head) {
     const { pathname } = url.parse(request.url);
