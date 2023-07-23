f016399002d4d294b0a013df8b17d2402a534bcd
radoneje <53825282+radoneje@users.noreply.github.com>
2023-07-23 23:39:29 +0300

    ee

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,5 +7,8 @@ res.render("index")
 router.get('/event/:id', (req, res, next)=> {
     res.render("event",{id:req.params.id})
 })
+router.get('/test', (req, res, next)=> {
+    res.render("test",{id:req.params.id})
+})
 
 module.exports = router;
