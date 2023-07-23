7d3f7cbc9e8cf3dc505fab3a997cc7c9015ae557
radoneje <53825282+radoneje@users.noreply.github.com>
2023-07-23 23:58:15 +0300

    ee

--- socket.js
+++ socket.js
@@ -4,14 +4,14 @@ const WebSocket = require('ws');
 const url = require('url');
 
 const server=http.createServer()
-const wss = new WebSocket({ noServer: true });
+const wss =  WebSocket.Server({ noServer: true });
 server.listen(7001);
 server.on('upgrade', function upgrade(request, socket, head) {
     const { pathname } = url.parse(request.url);
     console.log(pathname)
     if (pathname === '/ws') {
         wss.handleUpgrade(request, socket, head, function done(ws) {
-            wss1.emit('connection', ws, request);
+            wss.emit('connection', ws, request);
         });
     }
 });
