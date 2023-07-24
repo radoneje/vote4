1dc9c15e0ecd1f045cf1a52c9607c14820e51453
radoneje <53825282+radoneje@users.noreply.github.com>
2023-07-24 17:18:16 +0300

    ee

---
+++ gateway.js
@@ -0,0 +1,15 @@
+const Gateway = require('micromq/gateway');
+
+// создаем гейтвей
+const gateway = new Gateway({
+    microservices: ['market'],
+    rabbit: {
+        url: "amqp://guest:guest@localhost:5672"
+    },
+});
+
+// создаем эндпоинт и делегируем его в микросервис market
+gateway.post('/market/buy/:id', (req, res) => res.delegate('market'));
+
+// слушаем порт и принимаем запросы
+gateway.listen(7002);
