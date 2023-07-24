2df7d9d2b3a245f416558c07e8ba2c3215e0e24e
radoneje <53825282+radoneje@users.noreply.github.com>
2023-07-24 17:20:15 +0300

    ee

--- gateway.js
+++ gateway.js
@@ -9,7 +9,9 @@ const gateway = new Gateway({
 });
 
 // создаем эндпоинт и делегируем его в микросервис market
-gateway.post('/market/buy/:id', (req, res) => res.delegate('market'));
+gateway.post('/market/buy/:id', (req, res) => {res.delegate('market')
+res.json(1)
+});
 
 // слушаем порт и принимаем запросы
 gateway.listen(7002);
