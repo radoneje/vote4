357e43b174c6aa31d3e0f3db94e868363b54571a
radoneje <53825282+radoneje@users.noreply.github.com>
2023-07-24 14:25:44 +0300

    ee

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/event', (req, res, next)=> {
- res.json({id:1})
+router.post('/event', async (req, res, next)=> {
+ if(!req.body.id)
+ {
+  return (await req.knex("t_events").insert(req.body, "*"))[0];
+ }
+
+ let id=req.body.id
+ delete req.body.id;
+ return (await req.knex("t_events").update(req.body, "*").where({id}))[0]
 })
 
 
