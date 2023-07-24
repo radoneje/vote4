8cc0a7b49cbbe4a7cf5937472fb8e16a9b7e563e
radoneje <53825282+radoneje@users.noreply.github.com>
2023-07-24 13:58:28 +0300

    ee

--- public/scripts/classes/socketClass.js
+++ public/scripts/classes/socketClass.js
@@ -35,7 +35,7 @@ class SocketClass {
     send= async (cmd, value)=>{
         try {
             if(this.socket.readyState>1)
-                await reconnect(this.eventid)
+                await this.reconnect(this.eventid)
             this.socket.send(JSON.stringify({cmd, value, id: this.id, eventid: this.eventid}))
         }
         catch (e) {
