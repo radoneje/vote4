1361fcfbda256ce8b6243b488965bc9431465caa
radoneje <53825282+radoneje@users.noreply.github.com>
2023-07-26 00:32:56 +0300

    ee

--- public/scripts/eventAdminScript.js
+++ public/scripts/eventAdminScript.js
@@ -3,44 +3,47 @@
 import chat from './classes/chatClass.js'
 import SocketClass from './classes/socketClass.js'
 
-let socket=null;
-let app=new Vue({
-    el:"#app",
-    data:{
-        section:"design",
+let socket = null;
+let app = new Vue({
+    el: "#app",
+    data: {
+        section: "design",
         event,
     },
-    methods:{
-        uploadFile:async function(resVariable, sect, evnt, clbk)
-        {
+    methods: {
+        uploadFile: async function (resVariable, sect, evnt, clbk) {
             let inp = document.createElement("input")
             inp.type = "file"
             inp.accept = "image/png, image/jpeg"
-            inp.style.display="none"
+            inp.style.display = "none"
             document.body.appendChild(inp)
             inp.click()
             inp.onchange = async () => {
                 document.body.removeChild(inp)
-                let file=inp.files[0]
+                let file = inp.files[0]
                 let formData = new FormData()
                 formData.append('file', file, file.name);
                 let xhr = new XMLHttpRequest();
                 xhr.open('POST', '/api/uploadFile');
                 xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                let progress = evnt.target.parentNode.querySelector(".progress")
                 xhr.upload.addEventListener("load", function () {
-                   // $(".meter").addClass("done");
-                    file
+                    if (progress)
+                        setTimeout(() => {
+
+                            progress.style.width = 0;
+                        }, 200)
                 });
                 xhr.upload.addEventListener("progress", function (event) {
-                    /*if (event.lengthComputable) {
+                    if (event.lengthComputable && progress) {
                         var complete = (event.loaded / event.total * 100 | 0);
-                        $('.meter').css('width', complete + '%');
-                    }*/
+                        progress.style.width= complete + '%';
+                    }
                 });
-                xhr.onload = xhr.onerror = function() {
+                xhr.onload = xhr.onerror = function () {
                     if (this.status == 200) {
-                        resVariable[sect]="/file/"+JSON.parse( xhr.response)
-                        if(clbk)
+                        resVariable[sect] = "/file/" + JSON.parse(xhr.response)
+                        if (clbk)
                             clbk()
                     } else {
 
@@ -50,28 +53,27 @@ let app=new Vue({
             }
             console.log(resVariable, evnt)
         },
-        onMessage:async function(cmd, value)
-        {
-            if(cmd=="changeEvent" && this.event.id==value.id){
+        onMessage: async function (cmd, value) {
+            if (cmd == "changeEvent" && this.event.id == value.id) {
                 console.log("ChanngeEvent!", value)
-                        for(let key of Object.keys(value)){
-                            if(key!="id")
-                                this.event[key]=value[key];
-                        }
+                for (let key of Object.keys(value)) {
+                    if (key != "id")
+                        this.event[key] = value[key];
+                }
             }
         },
         changeEvent: async function (event, sect) {
-            let dt={id:event.id};
-            dt[sect]=event[sect]
+            let dt = {id: event.id};
+            dt[sect] = event[sect]
             let r = await postJson("/api/changeEvent", dt)
         },
-        socketSend:function (cmd,data){
+        socketSend: function (cmd, data) {
             socket.send(cmd, data)
         }
     },
-    mounted:async function () {
+    mounted: async function () {
         console.log("ready")
-        socket=new SocketClass(this.event.short, document.getElementById("app").getAttribute("userid"), this.onMessage)
+        socket = new SocketClass(this.event.short, document.getElementById("app").getAttribute("userid"), this.onMessage)
     }
 
 })
