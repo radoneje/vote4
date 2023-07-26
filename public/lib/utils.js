a4e478449002215bcd18c13304f8ed8cfdcacb6d
radoneje <53825282+radoneje@users.noreply.github.com>
2023-07-26 09:41:30 +0300

    ee

--- public/lib/utils.js
+++ public/lib/utils.js
@@ -40,3 +40,10 @@ window.download=(href, filename)=>{
     },1000)
 
 }
+window.validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
